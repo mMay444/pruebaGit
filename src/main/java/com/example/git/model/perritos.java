ab4091e0647785c2ed338d1da1b11b7ba07445fe
Jorge <jorspoty28@gmail.com>
2026-06-20 16:53:17 -0300

    perrito actualizado

--- src/main/java/com/example/git/model/perritos.java
+++ src/main/java/com/example/git/model/perritos.java
@@ -1,4 +1,37 @@
 package com.example.git.model;
 
 public class perritos {
+    private String nombre;
+    private int edad;
+    private String raza;
+
+    public perritos(String nombre, int edad, String raza) {
+        setNombre(nombre);
+        setEdad(edad);
+        setRaza(raza);
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
 }
